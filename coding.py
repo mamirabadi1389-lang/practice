7600f10cf7d04a4e0933286c929404b4c67e35fe
Mohammad <you@example.com>
2026-06-13 11:30:24 +0330

    project initialize

---
+++ coding.py
@@ -0,0 +1,15 @@
+class hello :
+    def __init__(self , helllo , godbay, entere ):
+        self.helllo = helllo 
+        self.godby = godbay
+        self.entere = entere
+
+    def __str__(self): 
+        return f"man grok hastam {self.helllo} agrt ba man kare nadare man beram {self.godby}"
+    def hesaban(self):
+        pass
+
+hhello = "salam"
+godby = "khodahafez"
+a = hello(hhello ,godby )
+print (a)
