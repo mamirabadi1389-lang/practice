27fef3c64246c69eea91861b86f82550e7c0cc06
devmofox_developer <mamirabadi1389@gmail.com>
2026-06-13 11:48:31 +0330

    Update coding.py
    update anjam shod

--- coding.py
+++ coding.py
@@ -1,15 +1,23 @@
-class hello :
-    def __init__(self , helllo , godbay, entere ):
-        self.helllo = helllo 
-        self.godby = godbay
-        self.entere = entere
+class Hello:
+    def __init__(self, hello, goodbye, enter):
+        self.hello = hello
+        self.goodbye = goodbye
+        self.enter = enter
 
-    def __str__(self): 
-        return f"man grok hastam {self.helllo} agrt ba man kare nadare man beram {self.godby}"
-    def hesaban(self):
-        pass
+    def __str__(self):
+        return f"man grok hastam {self.hello}. agrt ba man kare nadare man beram {self.goodbye}"
 
-hhello = "salam"
-godby = "khodahafez"
-a = hello(hhello ,godby )
-print (a)
+    # اضافه کردن قابلیت ماشین حساب
+    def hesaban(self, num1, num2):
+        # اینجا یک عملیات ساده جمع قرار دادیم
+        result = num1 + num2
+        return f"نتیجه محاسبات شما برابر است با: {result}"
+
+# مقداردهی درست (باید ۳ پارامتر مطابق __init__ داده شود)
+a = Hello("salam", "khodahafez", "ورود")
+
+# چاپ رشته کلاس
+print(a)
+
+# استفاده از ماشین حساب
+print(a.hesaban(10, 20))
